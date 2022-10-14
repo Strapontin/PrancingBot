45b28560f5b5c1842da62e460109546b77b20376
Anthony DELORME <antodu73@hotmail.fr>
2022-10-14 16:33:12 +0200

    Project created

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
+using prancing_bot.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace prancing_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        internal static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "__token__",
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged,
+                MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Debug,
+            });
+
+            // Allows us to use modules in Command folder
+            var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
+            {
+                StringPrefixes = new[] { "!" }
+            });
+            commands.RegisterCommands<GeneralModule>();
+
+            commands.SetHelpFormatter<Help>();
+
+            discord.UseInteractivity(new InteractivityConfiguration()
+            {
+                PollBehaviour = DSharpPlus.Interactivity.Enums.PollBehaviour.KeepEmojis,
+            });
+
+            discord.ComponentInteractionCreated += async (discordClient, componentInteractionCreateEventArgs) =>
+            {
+                if (componentInteractionCreateEventArgs.Id.Contains("my_very_cool_button"))
+                {
+                    // Renvoie un message lié à celui possédant les boutons
+                    await componentInteractionCreateEventArgs.Interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent("Cool"));
+                }
+                else if (componentInteractionCreateEventArgs.Id == "1_top")
+                {
+                    await componentInteractionCreateEventArgs.Interaction.CreateResponseAsync(InteractionResponseType.UpdateMessage, new DiscordInteractionResponseBuilder().WithContent("No more buttons for you >:)"));
+                }
+                else
+                {
+                    await componentInteractionCreateEventArgs.Interaction.CreateResponseAsync(InteractionResponseType.DeferredMessageUpdate);
+                }
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
